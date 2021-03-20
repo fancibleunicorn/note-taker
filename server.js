930bff7f35f105fa819879690d2a821b40aee19b
Adam Crandall <adamcrandall91@gmail.com>
2021-03-20 17:23:34 -0400

    css notes issue

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use('/', htmlRoutes);
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
