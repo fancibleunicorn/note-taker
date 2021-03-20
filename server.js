4fb6988295a44981607fc17ad40c2d9d41eed2fd
Adam Crandall <adamcrandall91@gmail.com>
2021-03-20 19:19:55 -0400

    routes are now working

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+
+app.use('/api', apiRoutes)
 app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
+;
 
 app.listen(3001, () => {
     console.log(`API server now on port ${PORT}!`);
