235fcd4186ac3f780ebb5371e2bff04654d5e8f8
Adam Crandall <adamcrandall91@gmail.com>
2021-03-20 17:31:39 -0400

    fixed css issue and html routes working

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.use('/', htmlRoutes);
 
 app.listen(3001, () => {
